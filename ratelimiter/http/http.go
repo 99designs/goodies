2534f09cae855adb7a9d3e09000314eb318cf98f
vinays <vinay.sekhri@gmail.com>
2013-07-09 14:45:13 -0700

    New Pull Request: Add Retry-After header to 429 response.

--- ratelimiter/http/http.go
+++ ratelimiter/http/http.go
@@ -2,7 +2,9 @@
 package http
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 const StatusTooManyRequests = 429
@@ -12,16 +14,17 @@ type RateLimitedHandler struct {
 	rateLimiter RateLimiter
 }
 
-type RateLimiter func(*http.Request) bool
+type RateLimiter func(*http.Request) (ok bool, retryAfter time.Duration)
 
 func Decorate(delegate http.Handler, rateLimiter RateLimiter) RateLimitedHandler {
 	return RateLimitedHandler{delegate, rateLimiter}
 }
 
 func (lh RateLimitedHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if lh.rateLimiter(r) {
+	if ok, retryAfter := lh.rateLimiter(r); ok {
 		lh.Handler.ServeHTTP(rw, r)
 	} else {
+		rw.Header().Set("Retry-After", fmt.Sprint(int64(retryAfter.Seconds())))
 		http.Error(rw, "Too many requests", StatusTooManyRequests)
 	}
 }
