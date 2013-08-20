f2eafe9684ce4334a100085df28e5029d05a9a38
Daniel Heath <daniel.r.heath@gmail.com>
2013-08-20 15:45:22 +1000

    Only send web requests to statsd if they are to a named route

--- monitor/statsd/http.go
+++ monitor/statsd/http.go
@@ -2,14 +2,17 @@ package statsd
 
 import (
 	"fmt"
+	"github.com/99designs/goodies/goanna"
+	"github.com/gorilla/mux"
 	"net/http"
 	"time"
 )
 
 type StatsdLogHandler struct {
-	handler http.Handler
-	statter *Statsd
-	prefix  string
+	handler          http.Handler
+	statter          *Statsd
+	prefix           string
+	RequestFormatter func(req *http.Request) string
 }
 
 // ServeHTTP records the time for a request and sends the result to statsd
@@ -18,15 +21,30 @@ func (sh *StatsdLogHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) 
 	sh.handler.ServeHTTP(w, req)
 	requestDuration := time.Now().Sub(requestStartTime)
 
-	description := fmt.Sprintf("%s.%s.%s.", sh.prefix, req.Method, req.URL.Path)
-	sh.statter.Counter(1.0, description+"requests", 1)
-	sh.statter.Timing(1.0, description+"responsetime", requestDuration)
+	description := sh.RequestFormatter(req)
+	if description != "" {
+		fmt.Println(sh.prefix + "." + description + ".requests")
+		sh.statter.Counter(1.0, sh.prefix+"."+description+".requests", 1)
+		sh.statter.Timing(1.0, sh.prefix+"."+description+".responsetime", requestDuration)
+	}
+}
+
+func defaultGetRequestString(req *http.Request) string {
+	var match mux.RouteMatch
+	if goanna.Router.Match(req, &match) {
+		name := match.Route.GetName()
+		if name != "" {
+			return fmt.Sprintf("%s.%s", req.Method, name)
+		}
+	}
+	return ""
 }
 
 func Decorate(statter *Statsd, prefix string, h http.Handler) http.Handler {
 	return &StatsdLogHandler{
-		handler: h,
-		statter: statter,
-		prefix:  prefix,
+		handler:          h,
+		statter:          statter,
+		prefix:           prefix,
+		RequestFormatter: defaultGetRequestString,
 	}
 }
