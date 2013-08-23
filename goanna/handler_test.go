30a236eef46ce7e0b4f619d576c6fa927d7dbe7f
Daniel Heath <daniel.r.heath@gmail.com>
2013-08-23 17:11:27 +1000

    Increase coverage

---
+++ goanna/handler_test.go
@@ -0,0 +1,55 @@
+package goanna
+
+import (
+	. "launchpad.net/gocheck"
+	"net/http"
+	"testing"
+	"time"
+)
+
+type HandlerSuite struct {
+	TestController
+}
+
+type TestController struct {
+	*Controller
+	Response
+}
+
+var _ = Suite(&HandlerSuite{})
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (s *HandlerSuite) TestCreateLoginToken(c *C) {
+	req := Must(http.NewRequest("GET", "/foo", nil))
+
+	factory := func() ControllerInterface { return &s.TestController }
+	h := NewHandler(factory, "ControllerMethod")
+
+	s.TestController.Controller = NewController(req, nil)
+	s.TestController.Response = NewResponse()
+	resp := h.getResponse(req)
+	c.Check(resp, Equals, s.TestController.Response)
+}
+
+func (s *TestController) Init(*http.Request)         {}
+func (s *TestController) Session() Session           { return NopSession{} }
+func (s *TestController) ControllerMethod() Response { return s.Response }
+
+func Must(r *http.Request, e error) *http.Request {
+	if e != nil {
+		panic(e)
+	}
+	return r
+}
+
+type NopSession struct {
+}
+
+func (s NopSession) Get(_ string) string             { return "" }
+func (s NopSession) Set(_string, _ string)           {}
+func (s NopSession) Expiry() time.Time               { return time.Now() }
+func (s NopSession) SetExpiry(_ time.Time) time.Time { return time.Now() }
+func (s NopSession) WriteToResponse(_ Response)      {}
+func (s NopSession) GetId() string                   { return "" }
+func (s NopSession) Clear()                          {}
