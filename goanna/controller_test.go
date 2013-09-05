8fc8a7713ea8b1a46cd9082133a3756154ed78aa
Daniel Heath <daniel.r.heath@gmail.com>
2013-09-05 16:15:07 +1000

    Add tests for debug logger

---
+++ goanna/controller_test.go
@@ -0,0 +1,64 @@
+package goanna
+
+import (
+	"bytes"
+	"fmt"
+	. "launchpad.net/gocheck"
+	"log"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+type ControllerSuite struct{}
+
+var _ = Suite(&ControllerSuite{})
+
+func TestErrorLogging(t *testing.T) { TestingT(t) }
+
+func (s *ControllerSuite) TestErrorLogging(c *C) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	con := NewController(req, nil)
+	output := bytes.Buffer{}
+	con.logger = log.New(&output, "", 0)
+	con.LogRequest()
+	fmt.Print(string(output.Bytes()))
+
+	startOfLog := `
+*** Diagnostic Log ***
+Url: /
+Method: GET
+Timestamp:`
+	out := string(output.Bytes())
+	c.Check(out, HasPrefix, startOfLog)
+	c.Check(out, Contains, "goanna/controller.go:76")
+}
+
+type SimpleChecker struct {
+	*CheckerInfo
+	CheckFn func([]interface{}, []string) (bool, string)
+}
+
+func (h SimpleChecker) Info() *CheckerInfo {
+	return h.CheckerInfo
+}
+
+func (h SimpleChecker) Check(params []interface{}, names []string) (result bool, error string) {
+	return h.CheckFn(params, names)
+}
+
+var HasPrefix = SimpleChecker{&CheckerInfo{Name: "HasPrefix", Params: []string{"obtained", "expected"}}, CheckHasPrefix}
+
+func CheckHasPrefix(params []interface{}, names []string) (result bool, error string) {
+	s := params[0].(string)
+	pre := params[1].(string)
+	return strings.HasPrefix(s, pre), ""
+}
+
+var Contains = SimpleChecker{&CheckerInfo{Name: "HasPrefix", Params: []string{"obtained", "expected"}}, CheckContains}
+
+func CheckContains(params []interface{}, names []string) (result bool, error string) {
+	s := params[0].(string)
+	pre := params[1].(string)
+	return strings.Contains(s, pre), ""
+}
