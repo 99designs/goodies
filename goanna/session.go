df5b68c5c721a8683f64e8d1765ec10979058c12
Michael Tibben <michael.tibben@99designs.com>
2013-09-19 11:59:59 +1000

    Update handler test with new session interface

--- goanna/session.go
+++ goanna/session.go
@@ -21,10 +21,10 @@ func NopSessionFinder(_ *Request) Session { return &NopSession{} }
 
 type NopSession struct{}
 
-func (s NopSession) Get(_ string) string             { return "" }
-func (s NopSession) Set(_string, _ string)           {}
-func (s NopSession) Expiry() time.Time               { return time.Now() }
-func (s NopSession) SetExpiry(_ time.Time) time.Time { return time.Now() }
-func (s NopSession) WriteToResponse(_ Response)      {}
-func (s NopSession) GetId() string                   { return "" }
-func (s NopSession) Clear()                          {}
+func (s NopSession) GetId() string              { return "" }
+func (s NopSession) Get(_ string) string        { return "" }
+func (s NopSession) Set(_string, _ string)      {}
+func (s NopSession) SetMaxAge(_ time.Duration)  {}
+func (s NopSession) HasExpired() bool           { return false }
+func (s NopSession) Clear()                     {}
+func (s NopSession) WriteToResponse(_ Response) {}
