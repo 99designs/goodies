e3ccd1a045cd997e7c73748fe57bc85ba76760ae
Daniel Heath <daniel.r.heath@gmail.com>
2013-09-20 13:19:34 +1000

    Much simpler statsd code with the latest g2s

--- monitor/statsd/statsd.go
+++ monitor/statsd/statsd.go
@@ -4,37 +4,18 @@ package statsd
 import (
 	"github.com/peterbourgon/g2s"
 	"log"
-	"time"
 )
 
 func Dial(proto, addr string) *Statsd {
 	st, err := g2s.Dial(proto, addr)
 
 	if err != nil {
-		log.Printf("Couldn't initiate statsd with address: '%s'", addr)
-		return &Statsd{nil}
+		log.Printf("Couldn't initiate statsd with address '%s' because %s", addr, err)
+		return &Statsd{g2s.Noop()}
 	}
 	return &Statsd{st}
 }
 
 type Statsd struct {
 	g2s.Statter
-}
-
-func (sd Statsd) Counter(sampleRate float32, bucket string, n ...int) {
-	if sd.Statter != nil {
-		sd.Statter.Counter(sampleRate, bucket, n...)
-	}
-}
-
-func (sd Statsd) Timing(sampleRate float32, bucket string, d ...time.Duration) {
-	if sd.Statter != nil {
-		sd.Statter.Timing(sampleRate, bucket, d...)
-	}
-}
-
-func (sd Statsd) Gauge(sampleRate float32, bucket string, value ...string) {
-	if sd.Statter != nil {
-		sd.Gauge(sampleRate, bucket, value...)
-	}
 }
